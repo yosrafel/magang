9eaef50781a3f0586564a5bd4192cce238a038b7
yosrafel <yos.kristanto@si.ukdw.ac.id>
2022-05-09 14:12:25 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,169 @@
+@extends('layout/master')
+@section('title', 'Home')
+@section('content')             
+
+<!-- SERVIS DAN GARANSI -->
+<!-- Judul -->
+<div class="section padding_layout_1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="main_heading text_align_left">
+            <h2>Servis & Garansi </h2>
+            <p class="large">Berikut ini adalah informasi tentang garansi dan servis</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ISI -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">PRODUK BERGARANSI RESMI</h4>
+            <p>Produk yang dijual di ELS Computer dilindungi oleh GARANSI RESMI, sehingga konsumen Els Computer tenang dan tidak perlu khawatir jika terjadi kerusakan bawaan dari pabrik.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">DURASI GARANSI</h4>
+            <p>
+              Untuk produk yang dituliskan di katalog produk selama waktu tertentu (misal 1 tahun) namun dari pabrik produsen menyatakan waktu lebih lama (misal 3 tahun), maka Els Computer bisa membantu untuk mengklaimkan garansi produk tersebut.
+              </br></br>Contoh: Memory SODIMM dari pabrik garansi lifetime, dan di ELS Computer dituliskan garansi 1 tahun. Maka jika ada klaim atas garansi sebelum 1 tahun dari pembelian, Els Computer bisa membantu mengurus klaim garansi tersebut di 
+              toko Els Computer terdekat. Sedangkan kalau sudah lebih dari 1 tahun, maka konsumen dipersilakan untuk klaim garansi pabrik di Service Center dari merk unit memory tersebut.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">ASUS SERVICE CENTER</h4>
+            <p>Els Computer adalah juga merupakan Authorized Service Center ASUS. Sehingga ELS Computer tidak hanya menjual produk ASUS namun juga bisa memperbaiki dengan standard layanan pabrikasi dari ASUS.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">SERVICE OUT OF WARRANTY</h4>
+            <p>
+              Untuk barang yang sudah lebih dari masa garansi, maka jika di klaim garansi maka akan masuk kategori SERVICE OUT OF WARRANTY (layanan berbayar); demikian pula apabila penyebab kerusakan di luar ketentuan yang dilindungi oleh garansi.
+              </br></br>Garansi berlaku terbatas, utamanya adalah yang disebabkan bawaan cacat pabrik sampai pada tingkatan toleransi tertentu. Detail luasan garansi bisa berbeda untuk tiap pabrikasi/merknya.
+              </br></br>Untuk layanan SERVICE OUT OF WARRANTY (layanan berbayar) di ELS Computer sementara ini hanya menerima untuk jenis produk LAPTOP saja.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading" style="text-align : center">DAFTAR SERVIS</h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">ACER</h4>
+            <p>Ruko Casagrande No.7 </br>Jl. Ringroad Utara, Yogyakarta </br>0274-871007/871116</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">ASUS</h4>
+            <p>Ruko Concat Square Kav.5. </br>Jl. Ringroad Utara, Yogyakarta</br>0274 -4469329</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">ASUS ELS</h4>
+            <p>Jl. C. Simanjuntak no. 38 Yogyakarta</br>0274-566569 EXT 106</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">BROTHER</h4>
+            <p>Jl. Kolonel Sigiono 63 Yogyakarta</br>0274 – 372614</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">EPSON</h4>
+            <p>Jl. Kolonel Sigiono 63 Yogyakarta</br>0274 – 372614</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">GALVA (projector)</h4>
+            <p>Jl. Laksda Yos Sudarso No.19, Yogyakarta</br>0274- 541946</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">HP</h4>
+            <p>Jl. Monjali, Yogyakarta</br>Sinduadi Mlati Sleman</br>0274-4532390/4532391</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">LENOVO</h4>
+            <p>Ruko YAP Square Blok B8</br>Jl. C. Simanjuntak, Yogyakarta</br>0274-3159220</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">LG</h4>
+            <p>Jl. Monjali No 78, YK</br>0274-882256</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">SAMSUNG</h4>
+            <p>Jl. Kaliurang KM 5, Yogyakarta</br>0274-581813/581815</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">WISNO GRAHACOMP (PRINTER CANON)</h4>
+            <p>Jl. Diponegoro No.62, Yogyakarta</br>0274-580620</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="full">
+          <div class="service_blog_inner">
+            <h4 class="service-heading">MSI</h4>
+            <p>Datasricpt Yogyakarta</br>Jl. Gadjah Mada 21 Kav. R-2</br>0274-266 48999</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- end section -->
+@endsection
